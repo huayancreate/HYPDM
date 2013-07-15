70a648dd652ba52d487c1f5a7d2d212203d33811
hywliu <wliu@huayancreate.com>
2013-07-15 18:01:12 +0800

    HYPDM源码更新

---
+++ src/HYPDM/HYPDM.Entities/PDM_VERSION_HISTORY.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using EAS.Data;
+using EAS.Data.Access;
+using EAS.Data.ORM;
+
+namespace HYPDM.Entities
+{
+   /// <summary>
+   /// 实体对象 PDM_VERSION_HISTORY(检入检出记录)。
+   /// </summary>
+   public partial class PDM_VERSION_HISTORY: DataEntity<PDM_VERSION_HISTORY>
+   {
+   
+   }
+}
