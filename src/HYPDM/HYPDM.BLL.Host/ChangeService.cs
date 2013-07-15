224dd4ac87fbc4f2cb9f8a3c69f9ee4c2c724ec4
hywliu <wliu@huayancreate.com>
2013-07-15 15:19:59 +0800

    HYPDM源码更新

---
+++ src/HYPDM/HYPDM.BLL.Host/ChangeService.cs
@@ -0,0 +1,14 @@
+﻿using EAS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HYPDM.BLL
+{
+    [ServiceObject("变更管理服务")]
+    [ServiceBind(typeof(IChangeService))]
+    public class ChangeService : BaseServiceObject, IChangeService
+    {
+    }
+}
---
+++ src/HYPDM/HYPDM.BLL.Host/VersionService.cs
@@ -0,0 +1,14 @@
+﻿using EAS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HYPDM.BLL
+{
+    [ServiceObject("版本管理服务")]
+    [ServiceBind(typeof(IVersionService))]
+    public class VersionService : BaseServiceObject, IVersionService
+    {
+    }
+}
