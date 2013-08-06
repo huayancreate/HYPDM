3cbb3b002e5856b96f9062b2e091bcbe1d60dc89
hywliu <wliu@huayancreate.com>
2013-08-06 10:00:45 +0800

    更新UI层代码
    更新UI层代码

---
+++ src/HYPDM/HYPDM.UI/BaseUI/BaseDetectionForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace HYPDM.WinUI.BaseUI
+{
+    partial class BaseDetectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.chkFileTransfer = new System.Windows.Forms.CheckBox();
+            this.chkCheckOut = new System.Windows.Forms.CheckBox();
+            this.chkOpenFile = new System.Windows.Forms.CheckBox();
+            this.btnSelectPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(77, 12);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(318, 21);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(77, 45);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(237, 21);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "文件";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "文件路径";
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(239, 123);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(75, 23);
+            this.btnDetect.TabIndex = 7;
+            this.btnDetect.Text = "检出";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(320, 123);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // chkFileTransfer
+            // 
+            this.chkFileTransfer.AutoSize = true;
+            this.chkFileTransfer.Location = new System.Drawing.Point(77, 84);
+            this.chkFileTransfer.Name = "chkFileTransfer";
+            this.chkFileTransfer.Size = new System.Drawing.Size(72, 16);
+            this.chkFileTransfer.TabIndex = 4;
+            this.chkFileTransfer.Text = "文件传输";
+            this.chkFileTransfer.UseVisualStyleBackColor = true;
+            // 
+            // chkCheckOut
+            // 
+            this.chkCheckOut.AutoSize = true;
+            this.chkCheckOut.Location = new System.Drawing.Point(155, 84);
+            this.chkCheckOut.Name = "chkCheckOut";
+            this.chkCheckOut.Size = new System.Drawing.Size(48, 16);
+            this.chkCheckOut.TabIndex = 5;
+            this.chkCheckOut.Text = "检出";
+            this.chkCheckOut.UseVisualStyleBackColor = true;
+            // 
+            // chkOpenFile
+            // 
+            this.chkOpenFile.AutoSize = true;
+            this.chkOpenFile.Location = new System.Drawing.Point(215, 84);
+            this.chkOpenFile.Name = "chkOpenFile";
+            this.chkOpenFile.Size = new System.Drawing.Size(72, 16);
+            this.chkOpenFile.TabIndex = 6;
+            this.chkOpenFile.Text = "执行应用";
+            this.chkOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectPath
+            // 
+            this.btnSelectPath.Location = new System.Drawing.Point(320, 43);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectPath.TabIndex = 3;
+            this.btnSelectPath.Text = "选择路径";
+            this.btnSelectPath.UseVisualStyleBackColor = true;
+            this.btnSelectPath.Click += new System.EventHandler(this.btnSelectPath_Click);
+            // 
+            // DetectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 157);
+            this.Controls.Add(this.btnSelectPath);
+            this.Controls.Add(this.chkOpenFile);
+            this.Controls.Add(this.chkCheckOut);
+            this.Controls.Add(this.chkFileTransfer);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "DetectionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "检出";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckBox chkFileTransfer;
+        private System.Windows.Forms.CheckBox chkCheckOut;
+        private System.Windows.Forms.CheckBox chkOpenFile;
+        private System.Windows.Forms.Button btnSelectPath;
+    }
+}
--- src/HYPDM/HYPDM.UI/Util/Common.cs
+++ src/HYPDM/HYPDM.UI/Util/Common.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections;
+﻿using EAS.Modularization;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
+using System.ComponentModel;
 using System.Data;
+using System.Drawing;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
+using EAS.Data.ORM;
+using HYPDM.Entities;
+using EAS.Services;
+using HYPDM.BLL;
+using HYPDM.WinUI.BaseUI;
+using EAS.Explorer;
+using AdvancedDataGridView;
+using System.IO;
+using System.Configuration;
+using System.Collections;
 
 namespace HYPDM.WinUI.Util
 {
     public class Common
     {
+        IAccount LoginInfo = EAS.Application.Instance.Session.Client as IAccount;
+        IPhysicalFileService _physicalService = ServiceContainer.GetService<IPhysicalFileService>();
+        IDocumentService _docService = ServiceContainer.GetService<IDocumentService>();
+
         public string GetExtensionToType(string exten)
         {
             var type = "";
@@ -56,10 +70,104 @@ namespace HYPDM.WinUI.Util
             var serverPort = Convert.ToInt32(ConfigurationManager.AppSettings["serverPort"]);
             var userName = ConfigurationManager.AppSettings["userName"].ToString();
             var passWord = ConfigurationManager.AppSettings["passWord"].ToString();
-            var file=ConfigurationManager.AppSettings["file"].ToString();
+            var file = ConfigurationManager.AppSettings["file"].ToString();
             FTPHelper helper = new FTPHelper(serverAddr, file, userName, passWord);
             return helper;
         }
 
+
+        /// <summary>
+        /// 检入
+        /// </summary>
+        public void CheckIn(string Id, string fileName)
+        {
+            HYPDM.Entities.PDM_PHYSICAL_FILE physicalfile = _physicalService.GetPhysicalFile(Id, "");
+            if (physicalfile == null) return;
+            BaseCheckInForm form = new BaseCheckInForm();
+            form.PhysicalFile = physicalfile;
+
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                physicalfile.FILESTATUS = "1";
+                var version = Convert.ToInt32(physicalfile.FILEVERSION);
+                physicalfile.FILEVERSION = (version + 1).ToString();
+                physicalfile.Save();
+
+                HYPDM.Entities.PDM_PHYSICAL_FILE file = form.PhysicalFile;
+                fileName = file.FILENAME;
+                VersionSave("1");
+            }
+        }
+        /// <summary>
+        /// 检出
+        /// </summary>
+        public void CheckOut(string Id, string fileName)
+        {
+            HYPDM.Entities.PDM_PHYSICAL_FILE physicalfile = _physicalService.GetPhysicalFile(Id, "");
+            if (physicalfile == null) return;
+            if (physicalfile.FILESTATUS == "0") { MessageBox.Show("当前文档已被检出，不能再次检出，请等待检出人检入！", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information); return; }
+            BaseDetectionForm form = new BaseDetectionForm();
+            form.PhysicalFile = physicalfile;
+
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                physicalfile.FILESTATUS = "0";
+                physicalfile.Save();
+
+                HYPDM.Entities.PDM_PHYSICAL_FILE file = form.PhysicalFile;
+                fileName = file.FILENAME;
+                VersionSave("0");
+            }
+        }
+        /// <summary>
+        /// 版本历史记录保存
+        /// </summary>
+        /// <param name="type">0：检出，1：检入</param>
+        private void VersionSave(string type)
+        {
+            PDM_VERSION_HISTORY version = new PDM_VERSION_HISTORY();
+            version.ID = _physicalService.GetMaxID().ToString();
+            version.VERSIONNO = DateTime.Now.ToString("yyyyMMddHHmmss");
+            version.VERSIONSTATUS = type;
+            version.OPERATORUSER = LoginInfo.LoginID;
+            version.OPERATORDATE = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            version.Save();
+        }
+
+        /// <summary>
+        /// 查看文件
+        /// </summary>
+        /// <param name="fileName">文件名称</param>
+        public void FileView(string fileName)
+        {
+            var temp = System.Environment.GetEnvironmentVariable("TEMP");
+            FTPHelper helper = FtpHepler();
+            var info = "";
+            var tempFilePath = temp + "\\" + fileName;
+            var result = helper.DownloadFile(temp, fileName, out info);
+            if (result)
+            {
+                System.Diagnostics.Process.Start(tempFilePath);
+            }
+            else
+            {
+                MessageBox.Show("文件查看失败,具体原因为：" + info, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="fileName">文件名称</param>
+        public void DownFile(string filePath, string fileName)
+        {
+            var info = "";
+            FTPHelper helper = FtpHepler();
+            var result = helper.DownloadFile(filePath, fileName, out info);
+            if (result)
+            {
+                MessageBox.Show("文件下载失败,具体原因为：" + info, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
