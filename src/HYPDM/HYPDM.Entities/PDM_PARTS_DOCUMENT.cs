9e9e9c594b5fdfa529844b6d301ada36fa8a2de5
bonze1983 <dswei@huayancreate.com>
2013-08-23 18:03:53 +0800

    半成品文档关联实体类

---
+++ src/HYPDM/HYPDM.Entities/PDM_PARTS_DOCUMENT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using EAS.Data;
+using EAS.Data.Access;
+using EAS.Data.ORM;
+
+namespace HYPDM.Entities
+{
+   /// <summary>
+   /// 实体对象 PDM_PARTS_DOCUMENT(半成品文档)。
+   /// </summary>
+   public partial class PDM_PARTS_DOCUMENT: DataEntity<PDM_PARTS_DOCUMENT>
+   {
+   
+   }
+}
