df925844e70fbf0261930ee687abbe022c723554
bonze1983 <dswei@huayancreate.com>
2013-08-24 22:41:01 +0800

    半成品文档对应操作接口及接口实现类

---
+++ src/HYPDM/HYPDM.BLL.Contracts/IPartsDocumentService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HYPDM.Entities;
+using EAS.Data.ORM;
+
+namespace HYPDM.BLL
+{
+    public interface IPartsDocumentService
+    {
+        int GetMaxID();
+
+        // 根据半成品ID查询数据
+        IList<PDM_PARTS_DOCUMENT> GetPartsDocList(string partsID);
+
+        // 根据文档ID查询数据
+        IList<PDM_PARTS_DOCUMENT> GetPartsDocByDocID(string docID);
+
+        // 保存半成品文档关联
+        void PartsDocSave(List<PDM_PARTS_DOCUMENT> partsDocList);
+
+        // 删除数据关联
+        void DelPartsDoc(List<PDM_PARTS_DOCUMENT> partsDocList);
+    }
+}
---
+++ src/HYPDM/HYPDM.BLL.Host/PartsDocumentService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EAS.Services;
+using HYPDM.Entities;
+using EAS.Data.ORM;
+using EAS.Data.Linq;
+using EAS.Data.Access;
+
+namespace HYPDM.BLL
+{
+    [ServiceObject("半成品文档关联服务管理")]
+    [ServiceBind(typeof(IPartsDocumentService))]
+    public class PartsDocumentService : BaseServiceObject, HYPDM.BLL.IPartsDocumentService
+    {
+        public int GetMaxID()
+        {
+            return new MaxCodeService().GetMaxCode(new PDM_PRODUCT_DOCUMENT().DbTableName);
+        }
+
+        public IList<PDM_PARTS_DOCUMENT> GetPartsDocList(string partsID)
+        {
+            DataEntityQuery<PDM_PARTS_DOCUMENT> query = DataEntityQuery<PDM_PARTS_DOCUMENT>.Create();
+
+            var p = (from item in query
+                     where (item.PARTSID == partsID)
+                     select item
+                );
+
+            return p.ToList();
+        }
+
+        public IList<PDM_PARTS_DOCUMENT> GetPartsDocByDocID(string docID)
+        {
+            DataEntityQuery<PDM_PARTS_DOCUMENT> query = DataEntityQuery<PDM_PARTS_DOCUMENT>.Create();
+
+            var p = (from item in query
+                     where (item.DOCID == docID)
+                     select item
+                );
+
+            return p.ToList();
+        }
+
+        #region 保存数据
+
+        public void PartsDocSave(List<PDM_PARTS_DOCUMENT> partsDocList)
+        {
+            this.DataAccessor.TransactionExecute(new TransactionHandler2(this.IniternalSave), partsDocList);
+        }
+
+        public void IniternalSave(IDataAccessor accessor, params object[] parameters)
+        {
+            IList<PDM_PARTS_DOCUMENT> partsDocList = parameters[0] as List<PDM_PARTS_DOCUMENT>;
+
+            foreach (PDM_PARTS_DOCUMENT partsDoc in partsDocList)
+            {
+                partsDoc.Save();
+            }
+        }
+        #endregion
+
+        #region 删除数据
+
+        public void DelPartsDoc(List<PDM_PARTS_DOCUMENT> partsDocList)
+        {
+            this.DataAccessor.TransactionExecute(new TransactionHandler2(IniternalDel), partsDocList);
+        }
+
+        public void IniternalDel(IDataAccessor accessor, params object[] parameters)
+        {
+            IList<PDM_PARTS_DOCUMENT> partsDocList = parameters[0] as IList<PDM_PARTS_DOCUMENT>;
+
+            foreach (PDM_PARTS_DOCUMENT partsDoc in partsDocList)
+            {
+                partsDoc.Delete();
+            }
+        }
+        #endregion
+    }
+}
