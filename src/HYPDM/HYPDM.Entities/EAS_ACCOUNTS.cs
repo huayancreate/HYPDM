a5572a602b6eee910775fb5023c7b9a5feca7dbd
崔胜 <scui@huayancreate.com>
2013-09-12 10:56:13 +0800

    更新entitys

---
+++ src/HYPDM/HYPDM.Entities/EAS_ACCOUNTS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using EAS.Data;
+using EAS.Data.Access;
+using EAS.Data.ORM;
+
+namespace HYPDM.Entities
+{
+   /// <summary>
+   /// 实体对象 EAS_ACCOUNTS(帐户信息)。
+   /// </summary>
+   public partial class EAS_ACCOUNTS: DataEntity<EAS_ACCOUNTS>
+   {
+   
+   }
+}
